f76eab140d0ccf4deb02172d0c4026ed08fe7b12
jpxllington <jjdp99@gmail.com>
2021-09-28 18:00:34 +0100

    add post functionality for limited search terms

--- client/index.js
+++ client/index.js
@@ -1,48 +1,31 @@
 
-
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-    searchterm = e.target.searchterm.value;
-    
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// "Below is non-functional code to try to implement the google search api"
 // const key = "AIzaSyDGxjb37qHVXzvLRNCqe3ibebG6kgt_Xcg";
 // const engineId = "3a3f3266232d4c34b"
 // let searchterm = "dog";
 
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchterm = e.target.searchterm.value;  
+   fetch('http://localhost:3000/', {
+            method:"POST",
+            body: JSON.stringify({searchterm: searchterm}),
+            headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(res => res.json())
+            .then(data => console.log(data))
+                     
+})
+        
+        
+        
+       // "Below is non-functional code to try to implement the google search api"  
 // function search(searchTerm){
-    //     url =`https://www.googleapis.com/customsearch/v1?q=${searchTerm}&cx=${engineId}&key=${key}`;
-    //     fetch(url)
+//         url =`https://www.googleapis.com/customsearch/v1?q=${searchTerm}&cx=${engineId}&key=${key}`;
+//         fetch(url)
 //         .then(res => res.json())
 //         .then(data => console.log(data))
 //         .then(data => processData(data))
@@ -51,12 +34,36 @@ form.addEventListener('submit', (e) => {
 // function processData(data){
 //     fetch("http://localhost:3000/",{
 //         method: "POST",
-//         body: JSON.stringify(data),
+//         body: JSON.stringify({data: data}),
 //         headers: {
 //             "Content-Type": "application/json"
 //         }
 //     })
 //     .then(res => res.json())
-// }
+
+
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // https://www.googleapis.com/customsearch/v1?q=cat&cx=3a3f3266232d4c34b&key=AIzaSyDGxjb37qHVXzvLRNCqe3ibebG6kgt_Xcg
--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors')
-
+const bodyParser = require('body-parser')
 
 const app = express();
-app.use(express());
+app.use(express.json());
 app.use(cors());
 
 app.get('/', (req,res) => {
@@ -11,7 +11,30 @@ app.get('/', (req,res) => {
 })
 
 app.post('/', (req,res) => {
-    
+    let choice = req.body.searchterm;
+    console.log(choice);
+    console.log(typeof(choice))
+    res.json({message: "working"})
+    if (choice === "hello"){
+        console.log(2342342)
+    }
+    switch(choice){
+        case"cat":
+            break;
+        case"dog":
+            break;
+        case"coffee":
+            break;
+        case'hello':
+            console.log(4)
+            break;
+        case"chicken":
+            break;
+
+        default:
+            break;
+    }
+        
     
 })
 
