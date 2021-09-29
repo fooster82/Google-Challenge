378919c53d8b90e36bb6ff00dcc1af155e4f05cd
jpxllington <jjdp99@gmail.com>
2021-09-29 11:21:17 +0100

    merge html files addability to display search results

--- client/index.js
+++ client/index.js
@@ -9,7 +9,7 @@
 const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    searchterm = e.target.searchterm.value;  
+    searchterm = e.target.query.value;  
 
     fetch('http://localhost:3000/', {
             method:"POST",
@@ -19,10 +19,26 @@ form.addEventListener('submit', (e) => {
                 }
             })
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => buildResults(data))
                      
 })
-        
+
+function buildResults(data){
+    document.querySelector('#homePage').style.display = 'none';
+    document.querySelector('#searchPage').style.display = 'block';
+    let searchResults = document.querySelector('.searchresults');
+    console.log(data)
+    for (let a= 0; a < (data.length); a++){
+        title = data[a]['title'];
+        link = data[a]['link'];
+
+
+        let para = document.createElement('p');
+        para.innerHTML = `<a href="${link}">${title}</a>`;
+        searchResults.append(para);
+    }
+
+}
         
         
        // "Below is non-functional code to try to implement the google search api"  
