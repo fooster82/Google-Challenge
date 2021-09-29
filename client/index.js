8326dd0577403df85b4ca0d130a8aff659e8e4b3
jpxllington <jjdp99@gmail.com>
2021-09-29 11:33:24 +0100

    functional search 'hard coded'

--- client/index.js
+++ client/index.js
@@ -6,8 +6,8 @@
 
 
 
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
+const homeform = document.querySelector('#Homesearch');
+homeform.addEventListener('submit', (e) => {
     e.preventDefault();
     searchterm = e.target.query.value;  
 
@@ -23,10 +23,28 @@ form.addEventListener('submit', (e) => {
                      
 })
 
+
+const searchform = document.querySelector('#Searchsearch');
+searchform.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchterm = e.target.query.value;  
+
+    fetch('http://localhost:3000/', {
+            method:"POST",
+            body: JSON.stringify({searchterm: searchterm}),
+            headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(res => res.json())
+            .then(data => buildResults(data))
+})
+
 function buildResults(data){
     document.querySelector('#homePage').style.display = 'none';
     document.querySelector('#searchPage').style.display = 'block';
     let searchResults = document.querySelector('.searchresults');
+    searchResults.innerHTML = "";
     console.log(data)
     for (let a= 0; a < (data.length); a++){
         title = data[a]['title'];
